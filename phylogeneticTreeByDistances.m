b8eb7addfeaca6f2174d89cc1661d0f797376cfa
Changchuan Yin <cyinbox@gmail.com>
2018-07-25 10:36:13 -0500

    Update phylogeneticTreeByDistances.m

--- phylogeneticTreeByDistances.m
+++ phylogeneticTreeByDistances.m
@@ -1,21 +1,24 @@
 % Plotting the phylogenetic tree by MATLAB using pair-wise distances.
 % 
 % Changchuan Yin, 07/22/2018
-% Univ. of Illinois at Chicago
+% University of Illinois at Chicago
 
 % 1. Get data from data file
+clear
+
 fileName='distances.txt';
 % Sample content in data file: pair-wise distances.
 %{
-A B 0.18
-A C 0.12
-A D 0.15
-B C 0.55
-B D 0.14
-C D 0.92
+gene A, gen B, 0.18
+gene A, gene C, 0.12
+gene A, gene D, 0.15
+gene B, gene C, 0.55
+gene B, gene D, 0.14
+gene C, gene D, 0.92
 %}
+
 fileID = fopen(fileName);
-data = textscan(fileID,'%s %s %f');
+data = textscan(fileID,'%s %s %f','Delimiter',',')
 n = length(data{1});        % All names in column 1
 
 % 2. Get the names in the order of distances and unique.
@@ -23,7 +26,6 @@ names = unique(data{1});
 last=length(names)
 nameLast = data{2}(n);      % And add the last element in column 2.
 names(last+1) = nameLast;
-%names = ['A','B','C','D']; % All names (column 1 and 2) in order.
 
 % 3. Populate the gene vector.
 for k = 1:length(names)
@@ -38,6 +40,6 @@ dists = data{3};
 UPGMAtree = seqlinkage(dists,'UPGMA',Genes);
 plot(UPGMAtree,'orient','left');
 xlabel('Dissimilarity distance', 'FontSize', 10,'FontWeight','bold')
-title('The phylogenetic tree of genomes A, B, C, and D', 'FontSize',10,'FontWeight','bold');
+title('The phylogenetic tree of genes A, B, C, and D', 'FontSize',10,'FontWeight','bold');
 
 fclose(fileID);
